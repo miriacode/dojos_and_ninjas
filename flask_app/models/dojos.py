4e04e4ff43056d430cb32a73e9fc26e999678f86
Miriam Acuña <82735526+miriacode@users.noreply.github.com>
2022-03-13 20:11:56 -0500

    Update dojos.py

--- flask_app/models/dojos.py
+++ flask_app/models/dojos.py
@@ -1,5 +1,4 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-#from flask_app.models.ninjas import Ninja
 
 class Dojo():
     def __init__(self, data):
@@ -8,7 +7,6 @@ class Dojo():
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
-        #ninjas = Ninja.get_ninjas_by_dojo_id({"dojo_id": id})
         self.ninjas = []
 
     #READ
@@ -19,21 +17,14 @@ class Dojo():
         dojos = []
         for dojo in result:
             dojos.append(cls(dojo))
-
         return dojos
 
     #READ (ONE)
     @classmethod
     def get_dojo_by_id(cls,data):
         #data = {'id': '1'}
-        #Me busca un salon en base a su id y me regresa una instancia de classroom
-
-        #Consulta en la base de datos el identificador que estuvieramos recibiendo
         query = 'SELECT * FROM dojos WHERE id = %(id)s'
         result = connectToMySQL('dojos_y_ninjas').query_db(query,data)
-        # [
-        #   {1,DC, (hora), (hora)}
-        # ]
         doj = result[0]
         dojo = cls(doj)
         return dojo
